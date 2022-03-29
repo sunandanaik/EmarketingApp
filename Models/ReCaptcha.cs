b1ad8f0d56f3e65a4397217b10d2ec714c98568d
Sunanda Naik <sungitmca@gmail.com>
2022-03-29 17:05:06 +0530

    recaptcha v2 done.

---
+++ Models/ReCaptcha.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+
+namespace EmarketingApp.Models
+{
+    public class ReCaptcha
+    {
+        public string Key = "6Ld3hAkfAAAAAE-EQlZ7mECazmFWykvmBBVT86tj";
+
+        public string Secret = "6Ld3hAkfAAAAAAqk2GMgAnC24XRTU6BCCIFYP0EJ";
+        public string Response { get; set; }
+    }
+}
